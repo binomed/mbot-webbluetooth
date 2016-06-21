0e0ee78ec38c178de022c4945bbefc357e3589b0
jefBinomed <jean.francois.garreau@gmail.com>
2016-06-21 22:03:08 +0200

    start to implement control from joystick

--- scripts/app.js
+++ scripts/app.js
@@ -9,50 +9,68 @@ function pageLoad() {
         console.error("No navigator.bluetooth found.");
         stepConnect.style.display = "none";
         noBluetooth.style.display = "flex";
-        
-        
-        
+
+
+
     } else {
         stepConnect.style.display = "flex";
         noBluetooth.style.display = "none";
         let mBot = require("./mbot/mbot");
-        
-        document.getElementById("connectBtn").addEventListener('click', _=>{            
+
+        document.getElementById("connectBtn").addEventListener('click', _ => {
             mBot.request()
-            .then(_=>{
-                return mBot.connect();
-            })
-            .then(_=>{
-                stepConnect.style.display = "none";
-                stepControl.style.display = "flex";
-                
-                let Joystick = require('./components/joystick.js');
-                new Joystick('joystick');
-                
-                let partJoystick = document.querySelector('.part-joystick');
-                let partBtn = document.querySelector('.part-button');
-                let switchParts = document.getElementById('switchParts');
-                switchParts.addEventListener('click', function(evt){
-                if (this.checked){
-                    partBtn.style.display = 'none';
-                    partJoystick.style.display = '';
-                }else{
-                    partBtn.style.display = '';
-                    partJoystick.style.display = 'none';
+                .then(_ => {
+                    return mBot.connect();
+                })
+                .then(_ => {
+                    stepConnect.style.display = "none";
+                    stepControl.style.display = "flex";
+
+                    let Joystick = require('./components/joystick.js');
+                    new Joystick('joystick', (data) => {
+                           mBot.processMotor(data.M1, data.M2);
+                    });
+
+                    let partJoystick = document.querySelector('.part-joystick');
+                    let partBtn = document.querySelector('.part-button');
+                    let switchParts = document.getElementById('switchParts');
+                    switchParts.addEventListener('click', function (evt) {
+                        if (this.checked) {
+                            partBtn.style.display = 'none';
+                            partJoystick.style.display = '';
+                        } else {
+                            partBtn.style.display = '';
+                            partJoystick.style.display = 'none';
+                        }
+                    });
                     
-                } 
-                });
-            })
+                    let btnUp = document.getElementById('btnUp');
+                    let btnDown = document.getElementById('btnDown');
+                    let btnLeft = document.getElementById('btnLeft');
+                    let btnRight = document.getElementById('btnRight');
+                    
+                    btnUp.addEventListener('touchstart', _=>{mBot.processMotor(-100,100) });
+                    btnDown.addEventListener('touchstart', _=>{mBot.processMotor(100,-100) });
+                    btnLeft.addEventListener('touchstart', _=>{mBot.processMotor(100,100) });
+                    btnRight.addEventListener('touchstart', _=>{mBot.processMotor(-100,-100) });
+                    
+                    btnUp.addEventListener('touchend', _=>{mBot.processMotor(0,0)});
+                    btnDown.addEventListener('touchend', _=>{mBot.processMotor(0,0)});
+                    btnLeft.addEventListener('touchend', _=>{mBot.processMotor(0,0)});
+                    btnRight.addEventListener('touchend', _=>{mBot.processMotor(0,0)});
+                    
+                    
+                })
         });
-		
-		let ColorPicker = require('./components/colorpicker.js');
-		new ColorPicker((rgb) => {
-			mBot.processColor(rgb.red, rgb.blue, rgb.green);
-		});
-        
-        
+
+        let ColorPicker = require('./components/colorpicker.js');
+        new ColorPicker((rgb) => {
+            mBot.processColor(rgb.red, rgb.blue, rgb.green);
+        });
+
+
     }
-	
+
 }
 
 
--- scripts/components/joystick.js
+++ scripts/components/joystick.js
@@ -1,25 +1,54 @@
-class Joystick{
+class Joystick {
 
-    constructor(id){
+    constructor(id, callback) {
         this.joystick = nipplejs.create({
-            zone:document.getElementById(id),
-            mode:'static',
-            position:{
+            zone: document.getElementById(id),
+            mode: 'static',
+            position: {
                 left: '50%',
-                top:'50%'
+                top: '50%'
             },
-            size:200,
-            color:'#3f51b5'
+            size: 200,
+            color: '#3f51b5'
         });
-        
-        this.joystick.on('move',this._move.bind(this))
+        this.callback = callback;
+
+        this.joystick.on('move', this._move.bind(this));
+        this.joystick.on('end', this._end.bind(this));
     }
-    
-    _move(evt, data){
+
+    _move(evt, data) {
+        if(data.direction){
+            switch(data.direction){
+                case 'up':                        
+                break;
+                case 'down':
+                break;
+                case 'left':
+                break;
+                case 'right':
+                break;
+                
+            }
+        }
+        /*this.callback({
+            x: data.position.x - data.instance.position.x,
+            y: data.position.y - data.instance.position.y,
+            distance : data.distance,
+            angle : data.direction ? data.direction.angle : null
+        });*/
+        
         console.debug(data);
-        
+
     }
     
+    _end(evt,data){
+        callback({
+            M1 : 0,
+            M2 : 0
+        });
+    }
+
 }
 
 module.exports = Joystick;
